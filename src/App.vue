214ef9e5f19067f05b845efc1b08ff16cbe39761
damascus08 <yusophh@gmail.com>
2022-04-28 14:30:59 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,230 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <h1>MUSIC PLAYER</h1>
+    </header>
+    <main>
+      <section class="player">
+        <!-- insert template string -->
+        <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+
+        <div class="controls">
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
+        </div>
+      </section>
+
+      <section class="playlist">
+        <h3>The Playlist</h3>
+            <!-- Loop songs object  -- set song src  -- set action on click -- bind class playing if true -->  
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src ? 'song playing' : 'song')">
+          {{ song.title }} - {{ song.artist }}
+        </button>
+      </section>
+    </main>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  export default {
+    name: 'app',
+    // setting component state
+    data () {
+      return {
+        current: {
+          title: 'Song title',
+          artist: 'Artist',
+        },
+        index: 0,
+        isPlaying: false,
+        songs: [
+          {
+            title: 'Grateful',
+            artist: 'Neffex',
+            src: require('./assets/neffex-grateful.mp3')
+          },
+          {
+            title: 'Invinsible',
+            artist: 'Deaf kev',
+            src: require('./assets/deaf-kev-invincible.mp3')
+          },
+          {
+            title: 'All my friends',
+            artist: 'Aj Mitchell',
+            src: require('./assets/aj_mitchell-all_my_friends.mp3')
+          },
+        ],
+        player: new Audio(),
+      }
+    },
+    // run this when component is created
+    created() {
+      this.current = this.songs[this.index];
+      this.player.src = this.current.src;
+      // this.player.play();
+    },
+    // add objects on methods
+    methods: {
+      play(song){
+        if( typeof song.src != 'undefined') {
+          this.current = song;
+          this.player.src = this.current.src;
+        }
 
-#nav {
-  padding: 30px;
+        this.player.play()
+        this.isPlaying = true;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+        //proceed to the next song after playing the current music 
+        this.player.addEventListener('ended', function(){
+          this.index++;
+          if(this.index > (this.songs.length - 1)) {
+            this.index = 0;
+          }
+          this.current = this.songs[this.index];
+          this.play =  this.current;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+        }.bind(this));
+
+      },
+      pause(){
+        this.player.pause();
+        this.isPlaying = false;
+      },
+      next(){
+        this.index++;
+        if(this.index > this.songs.length - 1) {
+          this.index = 0;
+        }
+
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      },
+      prev(){
+        this.index--;
+        if(this.index < 0) {
+          this.index = this.songs.length - 1;
+        }
+
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      }
+    },
   }
-}
+</script>
+
+<style>
+
+   /* part 1; */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+  }
+
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+    background-color: #212121;
+    color: #fff;
+    font-style: italic;
+  }
+
+   /* part 2 */
+  main {
+    width: 100%;
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 25px;
+  }
+
+  .song-title {
+    color: #53565a;
+    font-size: 32px;
+    font-weight: 700;
+    text-transform: uppercase;
+    text-align: center;
+  }
+
+  .song-title span {
+    font-weight: 400;
+    font-style: italic;
+  }
+
+  .controls {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 30px 15px;
+  }
+
+  button {
+    appearance: none;
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+
+  button:hover {
+    opacity: 0.8;
+  }
+
+  .play, .pause {
+    font-size: 20px;
+    font-weight: 700;
+    padding: 15px 25px;
+    margin: 0px 15px;
+    border-radius: 8px;
+    color: #fff;
+    background-color: #cc2e5d;
+  }
+
+  .next, .prev {
+    font-size: 16px;
+    font-weight: 700;
+    padding: 10px 20px;
+    margin: 0px 15px;
+    border-radius: 6px;
+    color: #fff;
+    background-color: #ff5858;
+  }
+
+  .playlist {
+    padding: 0px 30px;
+  }
+
+  .playlist h3 {
+    color: #212121;
+    font-size: 28px;
+    font-weight: 400;
+    margin-bottom: 30px;
+    text-align: center;
+  }
+
+  .playlist .song {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    font-size: 20px;
+    font-weight: 700;
+    cursor: pointer;
+  }
+
+  .playlist .song:hover {
+    color: #ff5858; 
+  }
+
+  .playlist .song.playing {
+    color: #fff;
+    background-image: linear-gradient(to right, #cc2e5d, #ff5858);
+
+  }
 </style>
